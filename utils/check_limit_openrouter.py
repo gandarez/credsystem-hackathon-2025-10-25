1de8e68051ed4fd5b13e2e14aac0b8354201c279
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-10-25 16:41:16 -0300

    Correções

--- utils/check_limit_openrouter.py
+++ utils/check_limit_openrouter.py
@@ -1,22 +1,11 @@
-import os
 import requests
 import json
-
-authorization = os.getenv("OPENROUTER_API_KEY")
-
-response    =  requests.get(
-    url     = "https://openrouter.ai/api/v1/key",
-    headers = {
-                  "Authorization": f"Bearer {authorization}"
-              }
+response = requests.get(
+  url="https://openrouter.ai/api/v1/key",
+  headers={
+    "Authorization": f"Bearer <seu_token_aqui>"
+  }
 )
-
 data = response.json()
-if 'error' in data and 'code' in data['error'] and data['error']['code'] != 200:
-    print(f"Error : {data['error']['message']}")
-    exit(1)
-
-usage = data['data']['usage']
-print(f'$ {usage:.2f} used')
-
-exit(0)
+data["data"]["usage"]
+print(f'${data["data"]["usage"]:.2f} used today.')
