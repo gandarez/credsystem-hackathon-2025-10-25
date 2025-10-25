74facd9e9e73cdda5b3867a133920a15400f6b93
TaysonMartinss <tayson.martins@sptech.school>
2025-10-25 12:35:41 -0300

    bruno vamp

--- utils/check_limit_openrouter.py
+++ utils/check_limit_openrouter.py
@@ -3,7 +3,7 @@ import json
 response = requests.get(
   url="https://openrouter.ai/api/v1/key",
   headers={
-    "Authorization": f"Bearer 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"
+    "Authorization": f"Bearer "
   }
 )
 data = response.json()
