85afbfdd8f4b0442c6dd162afd410fbf56c2d01e
Mauricio Lima <mauricio.lima@loccus.com.br>
2025-10-25 15:04:33 -0300

    Melhorias no utilitario para consultar consumo do Open Router - Leitura da API Key de variável de ambiente

--- utils/check_limit_openrouter.py
+++ utils/check_limit_openrouter.py
@@ -1,11 +1,22 @@
+import os
 import requests
 import json
-response = requests.get(
-  url="https://openrouter.ai/api/v1/key",
-  headers={
-    "Authorization": f"Bearer <seu_token_aqui>"
-  }
+
+authorization = os.getenv("OPENROUTER_API_KEY")
+
+response    =  requests.get(
+    url     = "https://openrouter.ai/api/v1/key",
+    headers = {
+                  "Authorization": f"Bearer {authorization}"
+              }
 )
+
 data = response.json()
-data["data"]["usage"]
-print(f'${data["data"]["usage"]:.2f} used today.')
+if 'error' in data and 'code' in data['error'] and data['error']['code'] != 200:
+    print(f"Error : {data['error']['message']}")
+    exit(1)
+
+usage = data['data']['usage']
+print(f'$ {usage:.2f} used')
+
+exit(0)
