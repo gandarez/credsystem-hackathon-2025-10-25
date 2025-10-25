660ed35378e011bcb7a9cb100191c2c41d9d1139
veronica.santos <veronica.santos@asaptech.io>
2025-10-25 16:32:35 -0300

    fix2

--- utils/check_limit_openrouter.py
+++ utils/check_limit_openrouter.py
@@ -1,9 +1,14 @@
 import requests
 import json
+import os
+key = os.environ["OPENROUTER_API_KEY"]
+
 response = requests.get(
   url="https://openrouter.ai/api/v1/key",
   headers={
-    "Authorization": f"Bearer <seu_token_aqui>"
+    "Authorization": f"Bearer {key}"
   }
 )
-print(json.dumps(response.json(), indent=2))
+data = response.json()
+data["data"]["usage"]
+print(f'${data["data"]["usage"]:.2f} used today.')
