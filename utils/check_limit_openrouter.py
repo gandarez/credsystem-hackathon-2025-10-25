c2f012490fdaf7e33b8488cef5e6de1458248898
Bruno Mendes <99292060+devbrunomendes@users.noreply.github.com>
2025-10-25 16:26:46 -0300

    fix3

--- utils/check_limit_openrouter.py
+++ utils/check_limit_openrouter.py
@@ -3,9 +3,7 @@ import json
 response = requests.get(
   url="https://openrouter.ai/api/v1/key",
   headers={
-    "Authorization": f"Bearer "
+    "Authorization": f"Bearer <seu_token_aqui>"
   }
 )
-data = response.json()
-data["data"]["usage"]
-print(f'${data["data"]["usage"]:.2f} used today.')
+print(json.dumps(response.json(), indent=2))
